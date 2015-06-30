a7198bcd6873e3fa2a75dcf23b687a7ae6605dad
kush <kushkumardhawan@gmail.com>
2015-06-30 05:34:12 +0530

    This is the eighth Commit  . Added Empty Screens to the Project . Trying to add a custom calender view in android and inside this project

---
+++ app/src/main/java/epds/guesthouse/Activity_Search.java
@@ -0,0 +1,17 @@
+package epds.guesthouse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_Search extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+
+
+
+}
---
+++ app/src/main/java/epds/guesthouse/Activity_Splash.java
@@ -0,0 +1,17 @@
+package epds.guesthouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Activity_Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+ 
+}
